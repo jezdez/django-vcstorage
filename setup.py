26fec6e182079f52e7bb163e3e0b2a084ed31f71
Jannis Leidel <jannis@leidel.info>
2009-06-09 15:02:09 +0200

    Added some default files

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-vcstorage",
+    version = "0.1",
+    url = 'http://bitbucket.org/jezdez/django-vcstorage/',
+    license = "BSD",
+    description = "",
+    long_description = read('README'),
+    author = 'Jannis Leidel',
+    author_email = 'jannis@leidel.info',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
